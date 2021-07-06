5905e89a6b74e6c45f4560ebe9cc913aa96d1741
Claudiu-Vlad Ursache <claudiu+hw@shiftleft.io>
2021-07-06 18:31:05 +0200

    Define more queries

--- cpgqls_client/queries.py
+++ cpgqls_client/queries.py
@@ -12,5 +12,25 @@ language=\"%s\")"""
     return u"importCode(\"%s\")" % (path)
 
 
+def open_query(project_name):
+    return f"open(\"{project_name}\")"
+
+
+def close_query(project_name):
+    return f"close(\"{project_name}\")"
+
+
+def delete_query(project_name):
+    return f"delete(\"{project_name}\")"
+
+
+def help_query():
+    return f"help"
+
+
 def workspace_query():
     return "workspace"
+
+
+def project_query():
+    return "project"
